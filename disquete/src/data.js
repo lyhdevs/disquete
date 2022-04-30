0648ebffa42fd1969b52082e6568a485ef0cf77a
bryan <xxbryan343@gmail.com>
2022-04-29 23:03:08 -0500

    botones funcionando

--- disquete/src/data.js
+++ disquete/src/data.js
@@ -1,21 +1,21 @@
 export const ordenar = {
-  az: function (pokemon) {
-    return pokemon.sort((pokemonA, pokemonB) => {
-      if (pokemonA.name > pokemonB.name) {
+  az: function (ropa) {
+    return ropa.sort((ropaA, ropaB) => {
+      if (ropaA.name > ropaB.name) {
         return 1;
       }
-      if (pokemonA.name < pokemonB.name) {
+      if (ropaA.name < ropaB.name) {
         return -1;
       }
       return 0;
     });
   },
-  za: function (pokemon) {
-    return pokemon.sort((pokemonA, pokemonB) => {
-      if (pokemonA.name < pokemonB.name) {
+  za: function (ropa) {
+    return ropa.sort((ropaA, ropaB) => {
+      if (ropaA.name < ropaB.name) {
         return 1;
       }
-      if (pokemonA.name > pokemonB.name) {
+      if (ropaA.name > ropaB.name) {
         return -1;
       }
       return 0;
@@ -23,22 +23,23 @@ export const ordenar = {
   },
 };
 export const functionFilter = {
-  tipos: function (pokemon, value) {
-    const filterT = pokemon.filter((item) => {
-      if (item.type.includes(value)) {
+
+  categoria: function (ropa, value) {
+    const filterR = ropa.filter((item) => {
+      if (item.category.includes(value)) {
+        return true
+      } else
+        return false;
+    })
+    return filterR;
+  },
+  estacion: function (ropa, value) {
+    const filterT = ropa.filter((item) => {
+      if (item.season.includes(value)) {
         return true
       } else
         return false;
     })
     return filterT;
   },
-  generacion: function (pokemon, value) {
-    const filterR = pokemon.filter((item) => {
-      if (item.generation.name.includes(value)) {
-        return true
-      } else
-        return false;
-    })
-    return filterR;
-  }
 }
